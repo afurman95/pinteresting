9bdf7b9ff0da6b00080b4562ef9227ce7ece77a9
Aryeh L. Furman <alfurman@indiana.edu>
2014-05-16 20:26:54 -0700

    new product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/product'
+
  root "pages#home"
  get "about" => "pages#about" 
+ get "product" => "pages#product"
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
